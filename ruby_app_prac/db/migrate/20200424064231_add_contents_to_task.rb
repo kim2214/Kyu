04344ee950f77066b99e9965b9ddc0f02883343b
KyuSoo Kim <anzel21@naver.com>
2020-07-06 16:08:51 +0900

    delete ruby prac

--- ruby_app_prac/db/migrate/20200424064231_add_contents_to_task.rb
+++
@@ -1,5 +0,0 @@
-class AddContentsToTask < ActiveRecord::Migration[5.2]
-  def change
-    add_column :tasks, :contents, :string
-  end
-end
--- ruby_app_prac/spec/factories/tasks.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :task do
-    title { 'test' }
-    contents { 'test contents' }
-  end
-end
